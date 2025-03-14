1a2801556ecd9a9dd1d4bd162c8726ab6b657abd
Ivailo <ivailorg@gmail.com>
2025-03-14 12:54:06 +0200

    First commit, uploading python backend with frontend on react and typescript with minimal design

---
+++ client/src/App.tsx
@@ -0,0 +1,150 @@
+import './App.css'
+import React from 'react'
+
+function App() {
+  return (
+    <div className="page-wrapper">
+      <Header />
+      <PlaylistContainer />
+    </div>
+  )
+}
+
+function Header() {
+
+  return (
+    <header>
+      <LoginButton />
+      <LogOutButton />
+    </header>
+  )
+}
+
+function LoginButton() {
+  const handleClick = () => {
+    window.location.href = 'http://127.0.0.1:8080/login';  // Open in the same tab
+  }
+
+  return (
+    <button onClick={handleClick}>Login</button>
+  )
+}
+
+function LogOutButton() {
+  const handleClick = () => {
+    window.location.href = 'http://127.0.0.1:8080/logout';
+  }
+
+  return (
+    <button onClick={handleClick}>Logout</button>
+  )
+}
+
+function PlaylistContainer() {
+
+  let initialList: any[] = [];
+  const [list, setList] = React.useState(initialList);
+
+  // Later add variable to send request based on the platform
+  const fetchPlaylists = async () => {
+    const response = await fetch('http://127.0.0.1:8080/getUserPlaylists', {
+      method: 'GET',
+      credentials: 'include',  // Include cookies in the request
+    });
+
+    const data = await response.json();
+    return data;
+  };
+
+  const handleClick = () => {
+    fetchPlaylists()
+      .then((data) => {
+        if (data['error']) {
+          console.log(data['error']);
+          return;
+        }
+        setList(data['items']);
+        console.log(data['items']);
+    }).catch((error) => {
+      console.error('Error:', error);
+    });
+  };
+
+  return (
+    <div className="playlist-wrapper">
+      <ul className="playlist-container">
+        <button onClick={handleClick}>Print Playlists</button>
+        {list.map((item) => (
+          <PlaylistItem playlist={item}/>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+function PlaylistItem({ playlist }: { playlist: any }) {
+
+  let trackList: any[] = [];
+  const [tracks, setTracks] = React.useState(trackList);
+  
+  const totalTracks = playlist.tracks['total'];
+
+  const imageSrc = (): string =>
+  {
+    if (playlist.images) {
+      return playlist.images[0].url;
+    }
+    else {
+      return 'song_cover.png';
+    }
+  }
+
+  const fetchPlaylistTracks = async (id: string) => {
+    const response = await fetch(`http://127.0.0.1:8080/getPlaylistTracks?playlistId=${id}&totalTracks=${totalTracks}`, 
+    {
+      method: 'GET',
+      credentials: 'include',
+      headers: 
+      {
+        'Content-Type': 'application/json'
+      }
+      });
+    
+    const data = await response.json();
+    return data;
+  }
+
+
+  const handleClick = async (id: string) => 
+  {
+    fetchPlaylistTracks(id)
+      .then((data) => {
+        if (data['error']) {
+          console.log(data['error']);
+          return;
+        }
+        setTracks(data['items']);
+        console.log(data['items']);
+    }).catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  return (
+    <div className="playlist-item">
+      <img src={imageSrc()} width={100} height={100} alt="playlist" />
+      <div>
+        <a target="_blank" href={playlist.external_urls['spotify']}>{playlist.name}</a>
+        <p>{playlist.tracks['total']} tracks</p>
+      </div>
+      <button onClick={() => handleClick(playlist.id)}>Get tracks</button>
+      <ul>
+        {tracks.map((track) => (
+          <li key={track.track.id}>{track.track.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
---
+++ server/main.py
@@ -0,0 +1,124 @@
+import spotipy
+import os
+from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, url_for, session, request, redirect, jsonify
+import time
+from flask_cors import CORS , cross_origin
+from spotipy.cache_handler import FlaskSessionCacheHandler
+
+SPOTIPY_REDIRECT_URI = 'http://127.0.0.1:8080/callback'
+SCOPES = ["playlist-read-private", "playlist-modify-private"]
+
+cacheHandler = FlaskSessionCacheHandler(session)
+
+# App config
+app = Flask(__name__)
+
+app.secret_key = os.urandom(64)
+app.config['SESSION_COOKIE_NAME'] = 'spotify-login-session'
+
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = True  # Only enable this in production with HTTPS
+
+cors = CORS(app, origins='http://localhost:5173', supports_credentials=True)
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+@app.route('/login')
+def login():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+@app.route('/callback')
+@cross_origin(methods=['POST'], supports_credentials=True, headers=['Content-Type', 'Authorization'], origin='http://127.0.0.1:5173')
+def authorize():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session["token_info"] = token_info
+    return redirect("http://localhost:5173/")
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('http://localhost:5173/')
+
+@app.route('/getUserPlaylists')
+def get_user_playlists():
+    if not isAuthorized():
+        return jsonify({'error': 'Invalid token'}), 401
+    
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+
+    playlists = sp.current_user_playlists()
+    if playlists is None:
+        return jsonify({'error': 'Invalid token'}), 401
+    else:
+        return playlists
+
+@app.route('/getPlaylistTracks')
+def get_playlist_tracks():
+    if not isAuthorized():
+        return jsonify({'error': 'Invalid token'}), 401
+    
+    playlistId = request.args.get('playlistId')
+    totalTracks = int(request.args.get('totalTracks'))
+
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    response = {'items': []}  # Initialize with an empty list for items
+
+    for i in range(0, totalTracks, 100):
+        chunk = sp.playlist_tracks(playlistId, fields='items(track(name, id, artists, album(name, images)))', offset=i, limit=100)
+        response['items'].extend(chunk['items'])  # Extend the items list with the new items
+
+    if response is None:
+        return jsonify({'error': 'Invalid playlistId'}), 400
+    else:
+        return jsonify(response)
+
+# Checks to see if token is valid and gets a new token if not
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    # Checking if the session already has a token stored
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    # Checking if token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    # Refreshing token if it has expired
+    if (is_token_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
+
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+            client_id=os.environ['SPOTIPY_CLIENT_ID'],
+            client_secret=os.environ['SPOTIPY_CLIENT_SECRET'],
+            redirect_uri=SPOTIPY_REDIRECT_URI,
+            cache_handler=cacheHandler,
+            scope=SCOPES)
+
+def isAuthorized():
+    session['token_info'], authorized = get_token()
+    session.modified = True
+    return authorized
+
+def start_flask_app():
+    app.run(port=8080)
+
+if __name__ == '__main__':
+    start_flask_app()
