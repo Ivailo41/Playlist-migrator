c499f5c2f4cae298e7d7ac422726e7c1aa822aa4
Ivailo <ivailorg@gmail.com>
2025-12-24 02:43:49 +0200

    Removing the root folder including from code 'hack' and adding it in the yml file.

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,12 +1,5 @@
-import sys
-import os
 import pytest
 from server.app import create_app
-
-# Add project root to sys.path for imports
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-if PROJECT_ROOT not in sys.path:
-    sys.path.insert(0, PROJECT_ROOT)
 
 
 @pytest.fixture
